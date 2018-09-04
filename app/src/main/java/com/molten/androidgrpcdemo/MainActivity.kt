066f9ee2170e789972f43eb5ca59fc8a025f81bc
Vivek Kartha <vivek.kartha@mobme.in>
2018-09-05 02:29:14 +0530

    Changed Rx implementation with Anko doAsync

--- app/src/main/java/com/molten/androidgrpcdemo/MainActivity.kt
+++ app/src/main/java/com/molten/androidgrpcdemo/MainActivity.kt
@@ -2,13 +2,9 @@ package com.molten.androidgrpcdemo
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import io.reactivex.Single
-import io.reactivex.SingleEmitter
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.schedulers.Schedulers
 import okhttp3.OkHttpClient
 import okhttp3.Request
-import tutorial.Dataformat
+import org.jetbrains.anko.doAsync
 
 class MainActivity : AppCompatActivity() {
     private val okHttpClient = OkHttpClient()
@@ -16,20 +12,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val url = "http://elyeproject.x10host.com/experiment/protobuf"
-        getPerson(url)
+        doAsync {
+            val person = getPerson(url)
+            println(person)
+        }
     }
 
-    private fun getPerson(url: String) {
-        Single.create<Dataformat.Person> { personApi(url, it) }
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribeOn(Schedulers.io())
-                .subscribe { t1, _ -> println(t1) }
-    }
-
-    private fun personApi(url: String, it: SingleEmitter<Dataformat.Person>) {
+    private fun getPerson(url: String): Person {
         val request = Request.Builder().url(url).build()
         val call = okHttpClient.newCall(request)
         val response = call.execute()
-        it.onSuccess(Dataformat.Person.parseFrom(response.body()!!.byteStream()))
+        return Person.parseFrom(response.body()!!.byteStream())
     }
 }
