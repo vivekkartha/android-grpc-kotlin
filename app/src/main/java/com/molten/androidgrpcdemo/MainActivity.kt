def5c6af60608a2dcad05fcc740b20140dc9f875
Vivek Kartha <vivek.kartha@mobme.in>
2018-09-05 18:14:57 +0530

    Added gRPC service call

--- app/src/main/java/com/molten/androidgrpcdemo/MainActivity.kt
+++ app/src/main/java/com/molten/androidgrpcdemo/MainActivity.kt
@@ -2,26 +2,24 @@ package com.molten.androidgrpcdemo
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import okhttp3.OkHttpClient
-import okhttp3.Request
+import io.grpc.okhttp.OkHttpChannelBuilder
 import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
 
 class MainActivity : AppCompatActivity() {
-    private val okHttpClient = OkHttpClient()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val url = "http://elyeproject.x10host.com/experiment/protobuf"
+        //Change to your local server's IP
+        val url = "localhost"
+        val emptyRequest: Empty = Empty.newBuilder().build()
+        val okHttpChannel = OkHttpChannelBuilder.forAddress(url, 8080)
+                .usePlaintext()
+                .build()
+        val personStub = PersonServiceGrpc.newBlockingStub(okHttpChannel)
         doAsync {
-            val person = getPerson(url)
-            println(person)
+            val person = personStub.getPerson(emptyRequest)
+            uiThread { println(person) }
         }
-    }
-
-    private fun getPerson(url: String): Person {
-        val request = Request.Builder().url(url).build()
-        val call = okHttpClient.newCall(request)
-        val response = call.execute()
-        return Person.parseFrom(response.body()!!.byteStream())
     }
 }
